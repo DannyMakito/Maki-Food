898c224819f7c91da99b21f6b6b8c618b46636bf
Makito <dmakito662@gmail.com>
2024-07-21 20:15:42 +0200

    I CREATED THE  Cartitem MODEL

---
+++ Online-Food-odering/src/main/java/com/maki/model/CartItem.java
@@ -0,0 +1,34 @@
+package com.maki.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JsonIgnore
+    private Cart cart;
+
+    @ManyToOne //many CI can have same food
+    private Food food;
+
+    private int qauntity;
+
+    private List<String> ingridients;
+
+    private int totalPrice;
+
+}
