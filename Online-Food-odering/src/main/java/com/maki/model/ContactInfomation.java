48a34cf5ea17533c92b51718c48a7103ceae8682
Makito <dmakito662@gmail.com>
2024-07-21 20:16:58 +0200

    I CREATED THE  contact infomation MODEL

---
+++ Online-Food-odering/src/main/java/com/maki/model/ContactInfomation.java
@@ -0,0 +1,17 @@
+package com.maki.model;
+
+
+import lombok.Data;
+
+@Data
+class ContactInfomation {
+
+    private String email;
+
+    private String mobile;
+
+    private String twitter;
+
+    private String intagram;
+
+}
