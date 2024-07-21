4426cc42c43239f5180d4f18cce513ac2b20239d
Makito <dmakito662@gmail.com>
2024-07-21 20:26:37 +0200

    I CREATED THE ORDER ITEM MODEL

---
+++ Online-Food-odering/src/main/java/com/maki/model/OrderItem.java
@@ -0,0 +1,29 @@
+package com.maki.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Food food;
+
+    private int quantity;
+
+    private Long totoalPrice;
+
+    private List<String> ingredeints = new ArrayList<>();
+}
