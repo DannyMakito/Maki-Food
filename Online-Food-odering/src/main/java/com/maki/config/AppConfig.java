84c79c02e363366da6e959e9b1ae5be0090364aa
Makito <dmakito662@gmail.com>
2024-07-21 20:14:02 +0200

    I CREATED THE SECURITY APP CONFIGURATION

---
+++ Online-Food-odering/src/main/java/com/maki/config/AppConfig.java
@@ -0,0 +1,70 @@
+package com.maki.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.tomcat.util.file.ConfigurationSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.AuthorizeHttpRequestsDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    SecurityFilterChain  securityFilterChain(HttpSecurity http) throws Exception{
+        http.sessionManagement( managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize -> Authorize
+                                .requestMatchers("/api/admin/**").hasAnyRole("RESTURENT_OWNER","ADMIN")
+                                .requestMatchers("/api/**").authenticated()
+                                .anyRequest().permitAll()
+                        ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                        .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+return null;
+    }
+        private CorsConfigurationSource corsConfigurationSource(){
+
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                CorsConfiguration  cfg = new CorsConfiguration();
+
+
+                //provid front-end url that can access backend
+                cfg.setAllowedOrigins(Arrays.asList(
+                        "https://maki-food.vercel.app",
+                        "http://localhost:3000"
+                ));
+
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
+
+
+                return cfg;
+            }
+
+             };
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+        }
+}
